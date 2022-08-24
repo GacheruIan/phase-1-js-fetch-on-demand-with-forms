6427d8aa65e89307c21436a9982dd2963ef86cba
Gacheruian <ian.gacheru@student.moringaschool.com>
2022-08-24 21:22:25 +0300

    Done the assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 const init = () => {
-  
-}
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event)=>{
+    event.preventDefault();
+                                                                                                    // const display = document.getElementById('searchByID').value
+                                                                                                    // console.log(display)
+    const input = document.querySelector('input#searchByID'); 
+    fetch(`http://localhost:3000/movies/${input.value}`)
+   .then (response => response.json())
+   .then (data => {                                                                                   //   .then (function(data){
+                                                                                                      //     console.log(data)
+                                                                                                      //   })
+                                                                                                     // }
+   const title = document.querySelector('section#movieDetails h4');
+   const summary = document.querySelector('section#movieDetails p');
+   title.innerText = data.title;
+   summary,innerText = data.summary;
+
+   } );                                                                                             
+});
+};
 
 document.addEventListener('DOMContentLoaded', init);
+
